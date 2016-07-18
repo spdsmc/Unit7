922ce7ab9f1c6982a4da023d0f286afd5e381f4e
spdsmc <stewart.dulaney@outlook.com>
2016-07-17 23:46:39 -0700

    Add files via GitHub website

---
+++ 5.27.js
@@ -0,0 +1,8 @@
+var Movie = function (title, rating, listDirectors, listProducers, studio, releaseDate) {
+    this.title = title;
+    this.rating = rating;
+    this.listDirectors = listDirectors;
+    this.listProducers = listProducers;
+    this.studio = studio;
+    this.releaseDate = releaseDate;
+}
